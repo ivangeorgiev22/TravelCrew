30648d3344c33f2ea93771c6039f0fe811d6321d
Sonia Lopez <s.lopez@montpellier-bs.com>
2026-03-12 18:05:39 +0100

    Test

---
+++ client/src/service-auth/auth.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import type {UserData} from '../types/userData'; 
+
+// Use the API from the Backend 
+// Mock Data for now 
+
+const API_URL = "http://localhost:3030/auth"; 
+
