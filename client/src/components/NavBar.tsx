4884937458fee1213f9600a35505bfce97d81189
Ivan Georgiev <i.georgiev2@icloud.com>
2026-03-16 12:27:47 +0000

    feat/avatar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,14 +1,26 @@
 import { useNavigate } from "react-router"
+import { useState } from "react";
+import { IoLogOut } from "react-icons/io5";
 
 export default function NavBar () {
 const navigate = useNavigate();
+const [userName] = useState(() => {
+  return localStorage.getItem("userName") || "";
+});
+
 return (
   <div className="bg-orange-500 flex p-3 justify-between">
     <div>
       <h1 onClick={() => navigate('/dashboard')} className="cursor-pointer">TravelCrew</h1>
     </div>
-    <div>
-      <button onClick={() => navigate('/')} className="cursor-pointer">Logout</button>
+    <div className="flex gap-2 items-center">
+      <div className="w-8 h-8 rounded-full bg-primary/10 flex items-center justify-center bg-white ">
+        <span className="text-sm font-body font-semibold text-primary">
+          {userName.charAt(0)}  
+        </span>
+      </div>
+      <span className="text-sm font-body text-foreground hidden sm:block">{userName}</span>
+      <button onClick={() => navigate('/')} className="cursor-pointer text-2xl"><IoLogOut /></button>
     </div>
   </div>
 )
--- client/src/services/auth.ts
+++ client/src/services/auth.ts
@@ -1,15 +1,13 @@
 import axios from "axios";
 import type { UserData } from "../types/userData";
 
-// Use the API from the Backend
 const apiUrl = "http://127.0.0.1:3000/auth";
-// Mock Data for now
 
 export const login = async (userData: UserData) => {
   const response = await axios.post(`${apiUrl}/login`, userData);
-  //const accessToken = response?.data?.accessToken;
   if (response.data.token) {
     localStorage.setItem("token", response.data.token);
+    localStorage.setItem('userName', response.data.name);
   }
   return response.data;
 };
@@ -20,4 +18,5 @@ export const register = async (userData: UserData): Promise<void> => {
 
 export const logout = () => {
   localStorage.removeItem("token");
+  localStorage.removeItem("userName");
 };
