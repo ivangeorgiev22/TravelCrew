2ceaf6f83694dd3d6d0eaa3312a263e8775aeb18
Ivan Georgiev <i.georgiev2@icloud.com>
2026-03-19 08:04:12 +0000

    animate/tripCard

--- client/src/components/TripCard.tsx
+++ client/src/components/TripCard.tsx
@@ -10,7 +10,7 @@ export default function TripCard({ trip }: TripCardProps) {
   const navigate = useNavigate();
 
   return (
-    <div onClick={() => navigate(`/trips/${trip.id}`)} className="bg-transparent rounded-xl flex flex-col w-70 cursor-pointer shadow-[0_10px_15px_rgba(0,0,0,0.50)]">
+    <div onClick={() => navigate(`/trips/${trip.id}`)} className="bg-transparent rounded-xl flex flex-col w-70 cursor-pointer shadow-[0_10px_15px_rgba(0,0,0,0.50)] transition delay-100 duration-200  hover:scale-110">
       <div className="">
         <img src={img} className="rounded-t-xl h-50 w-70"/>
       </div>
